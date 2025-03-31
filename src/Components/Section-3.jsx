90d37e4a01271f2b889ffe2d0a5dc07a43c5d5dd
miyemicodes <lizzymiyemi@gmail.com>
2025-03-31 17:00:01 +0100

    Update

--- src/Components/Section-3.jsx
+++ src/Components/Section-3.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-import DogSnack from "../assets/Dog-snack.png";
+import Snack from "../assets/Snack.png";
+import DogSnack from "../assets/Dodsnack.png";
 
 export default function Section3() {
 	return (
 		<section>
-			<div className="flex items-center justify-center">
+			<div className="flex flex-col items-center justify-center">
 				<div className="md:w-[570px] md:h-[570px]">
 					<img
 						src={DogSnack}
@@ -15,32 +16,36 @@ export default function Section3() {
 				</div>
 				<div className="flex flex-col items-start justify-start gap-4.5">
 					<h2 className="font-semibold text-[40px] leading-[120%] tracking-[0.25px] text-[#161723] ">
-						Nutrition is the foundation for longer, healthier lives in dogs.
+						Improve overall gastrointestinal health for better nutrient
+						absorption
 					</h2>
 					<p className="font-normal text-[16px] leading-[150%] tracking-[0.5px] text-[#424153] ">
-						Invest in your dog's future with our scientifically formulated
-						superfood-powered supplements. Give them the nutrition they deserve
-						and watch them thrive with vitality, energy, and the joy of a
-						longer, healthier life.
+						Through rigorous scientific studies and consultations with
+						veterinarians, we have created a breakthrough formula exclusively
+						tailored to combat the health challenges prevalent in dogs. A
+						staggering 91% of our customers have reported significant
+						improvements in their dogs' health after incorporating our product
+						into their diet.
 					</p>
 				</div>
-      </div>
-      
-			<div className="flex items-center justify-center">
+			</div>
+
+			<div className="flex flex-col items-center justify-center">
 				<div className="flex flex-col items-start justify-start gap-4.5">
 					<h2 className="font-semibold text-[40px] leading-[120%] tracking-[0.25px] text-[#161723] ">
-						Nutrition is the foundation for longer, healthier lives in dogs.
+						Prebiotics nourish the beneficial gut bacteria, supporting digestive
+						health
 					</h2>
 					<p className="font-normal text-[16px] leading-[150%] tracking-[0.5px] text-[#424153] ">
-						Invest in your dog's future with our scientifically formulated
-						superfood-powered supplements. Give them the nutrition they deserve
-						and watch them thrive with vitality, energy, and the joy of a
-						longer, healthier life.
+						Our dog food formula contains carefully selected prebiotics that
+						work in harmony with the gut microbiota, providing the necessary
+						nutrients for the growth and maintenance of beneficial bacteria,
+						ultimately supporting digestive health.
 					</p>
 				</div>
 				<div className="md:w-[570px] md:h-[570px]">
 					<img
-						src={DogSnack}
+						src={Snack}
 						alt="Happy Dog Bites"
 						className="md:w-[570px] md:h-[570px]"
 					/>
